086e8789d31dcbdd00c3317e9799fd3947bbd83a
lanyuliuyun <lanyuliuyun@foxmail.com>
2021-10-22 22:32:06 +0800

    update cap_play

--- player/cap_play.cc
+++ player/cap_play.cc
@@ -2,44 +2,68 @@
 #include "audio_cap_source_mf.h"
 #include "audio_play_mf.h"
 
+#include <Objbase.h>
+#include <mfapi.h>
+
 #include <list>
 #include <mutex>
-#include <stdio.h>
+#include <functional>
+
+class AudioSampleQueue
+{
+public:
+    AudioSampleQueue() {}
+    ~AudioSampleQueue() {
+        for (IMFSample* s : samples_)
+        {
+            s->Release();
+        }
+        samples_.clear();
+    }
+
+    void put(IMFSample* sample)
+    {
+        std::lock_guard<std::mutex> guard(samples_lock_);
+        sample->AddRef();
+        samples_.push_back(sample);
+    }
+
+    IMFSample* get()
+    {
+        IMFSample *sample = NULL;
+        std::lock_guard<std::mutex> guard(samples_lock_);
+        if (!samples_.empty())
+        {
+            sample = samples_.front();
+            samples_.pop_front();
+        }
+        return sample;
+    }
+private:
+    std::list<IMFSample*> samples_;
+    std::mutex samples_lock_;
+};
 
 int wmain(int argc, wchar_t *argv[])
 {
-    std::list<IMFSample*> samples;
-    std::mutex samples_lock;
+    CoInitializeEx(0, COINIT_MULTITHREADED);
+    MFStartup(MF_VERSION);
 
-    AudioCapSourceMF cap([&samples, &samples_lock](IMFSample* sample){
-        std::lock_guard<std::mutex> guard(samples_lock);
-        samples.push_back(sample);
-    });
+    AudioSampleQueue queue;
 
-    AudioPlayMF play([&samples, &samples_lock]() -> IMFSample* {
-        IMFSample *sample = NULL;
-        std::lock_guard<std::mutex> guard(samples_lock);
-        if (!samples.empty())
-        {
-            sample = samples.front();
-            samples.pop_front();
-        }
-        return sample;
-    });
-    
-    play.start();
+    AudioCapSourceMF cap(std::bind(&AudioSampleQueue::put, &queue, std::placeholders::_1));
+    AudioPlayMF play(std::bind(&AudioSampleQueue::get, &queue));
+
     cap.start();
-    
+    play.start();
+
     getchar();
     
     cap.stop();
-    play.start();
-    
-    for (IMFSample* s : samples)
-    {
-        s->Release();
-    }
-    samples.clear();
+    play.stop();
+
+    MFShutdown();
+    CoUninitialize();
 
     return 0;
 }
