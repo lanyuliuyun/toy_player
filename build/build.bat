bccaa740c927cbbad6bc4c13ea6d2f491c771e0e
lanyuliuyun <lanyuliuyun@foxmail.com>
2022-03-27 22:19:08 +0800

    update gitignore and build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,5 +1,8 @@
 @echo off
 
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake .. ^
+    -DLY_ROOT=F:/opensource/libyuv ^
+    -DFFMPEG_ROOT=D:/ffmpeg-n4.4-178-g4b583e5425-win64-gpl-shared-4.4 ^
+    -DOPUS_ROOT=F:/opensource/opus-v1.3.1
 
 cmake --build . --config Debug;CharacterSet=Unicode
